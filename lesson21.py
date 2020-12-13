1f069805c4f90d108f815ebc99db021ed1b7bfbd
Hayk258 <hayk-00@mail.ru>
2020-12-13 19:35:37 +0500

    function

---
+++ lesson21.py
@@ -0,0 +1,92 @@
+# def res():
+# 	print('Hi')
+# res()
+
+
+# def hello(x,y):
+# 	print(x)
+# 	print(y)
+# 	print(x+y)
+# 	print(x-y)
+# 	print(x*y)
+
+# hello(7,5)
+
+
+
+# def name(child2,child1):
+# 	print("the yongs child ",child2)
+# name(child1 = 'Ani',child2 = 'Jon')
+
+# def myfunction(country = 'Armeni'):
+# 	print('mi country is ',country)
+# myfunction('Vrastan')
+# myfunction('iran')
+# myfunction()
+
+
+# def my_function(Food):
+# 	for x in Food:
+# 		print(x,)
+# fruits = ['apple','orange','banana']
+# my_function(fruits)		
+
+
+
+
+
+# def my_function(x):
+# 	return 5 * x
+# """returnic heto kody chi ashxatum"""	
+# print(my_function(3))
+
+
+# z = 7
+# def gumarum():
+# 	global x
+# 	x = 5
+# 	return z
+
+# res = gumarum()
+
+# print(res)
+	
+# print(x)
+
+
+# def res(*args):
+# 	print(args)
+# 	'''*@ sarguma tuple '''
+# res(1,2,3,4,5,6,7)
+
+
+# def res(**kwargs):
+# 	print(kwargs)
+# '''** sargum enq dictionary'''
+# res(name='Hayk',age=18)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

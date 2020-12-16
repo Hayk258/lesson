12de8fd6594f50b51adfe09afb76ac81211acfbc
Hayk258 <hayk-00@mail.ru>
2020-12-16 19:23:55 +0500

    lesson22

---
+++ lesson22.py
@@ -0,0 +1,76 @@
+# def km_to_ml(km):
+# 	return km/1.6
+
+# km = int(input('km: '))
+# print(km_to_ml(km))
+
+
+# def days_to_second(days):
+# 	res = days * 24 * 60 * 60
+# 	return res
+
+# days = int(input('days: '))
+# print(days_to_second(days))
+
+
+
+
+# def valid_password(password):
+# 	count = 0
+# 	number = 0
+# 	if len(password) >= 8:
+# 		for i in password:
+# 			if i.isdigit():
+# 				number += 1
+# 			elif i.isalpha():
+# 				count += 1
+# 		if count > 1 and number > 1 and password[0].isupper():
+# 			''' i.isupper() mer tary mecatara sargum'''
+# 			return 'your password is srtong'
+# 		else:
+# 			return 'your password is not strong'
+# 	else:
+# 		return 'your password length is not 8'
+
+# password = input('please enter password: ')
+# print(valid_password(password))
+
+
+
+
+
+# word = 'hello'
+# print(word,'after upper metod',word.upper())
+# print(word,'after lower metod',word.lower())
+
+
+# import math 
+
+# def fac(n):
+# 	res = math.factorial(n)
+# 	return res
+
+# n = int(input('your factorial number: '))
+# print(fac(n))
+
+
+
+# def max_list(mylist):
+# 	mylist.sort()
+# 	return mylist[-1]
+
+
+# mylist = [5,6,12,67,3,9,]
+# print(max_list(mylist))
+
+
+
+
+
+
+
+
+
+
+
+
